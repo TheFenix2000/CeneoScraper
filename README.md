1a35697c60a76bd2a3da4c5334f23efea48aebe7
TheFenix2000 <m.wos.biznes@gmail.com>
2023-04-16 17:34:39 +0200

    2023-04-16 MW

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # CeneoScraper
 A simple web scrapper written in python
+
+## Table of CSS Selectors and Variale Names
+
+| **Składowa** | **Nazwa** | **Selektor** |
+| --- | --- | --- |
+| **Opinia** | opinion | div.js\_product-review |
+| **Identyfikator opinii** | opinion\_id | [data-entry-id] |
+| **Autor** | author | span.user-post\_\_author-name |
+| **Rekomendacja** | recommendation | span.user-post\_\_author-recomendation \> em |
+| **Liczba gwiazdek** | score | span.user-post\_\_score-count |
+| **Czy opinia jest potwierdzona zakupem** | purchased | div.review-pz |
+| **Data wystawienia opinii** | opinion\_date | span.user-post\_\_published \> time:nth-child(1)[datetime] |
+| **Data zakupu produktu** | purchase\_date | span.user-post\_\_published \> time:nth-child(2)[datetime] |
+| **Ile osób uznało opinię za przydatną** | likes | button.vote-yes[data-total-vote] |
+| **Ile osób uznało opinię za nieprzydatną** | dislikes | button.vote-no[data-total-vote] |
+| **Treść opinii** | content | div.user-post\_\_text |
+| **Wady** | cons | div.review-feature\_\_title—negatives ~ review-feature\_\_item |
+| **Zalety** | pros | div.review-feature\_\_title—positives ~ review-feature\_\_item |
--- scraper.py
+++ scraper.py
@@ -1,8 +1,36 @@
 import requests
+import json
+from bs4 import BeautifulSoup
+
+def get_cos():
+    pass
+
 # product_code = input("Enter Product Code: ")
 product_code = "113897315"
-# url = "https://www.ceneo.pl/" + product_code + "#tab=reviews"
-url = f"https://www.ceneo.pl/{product_code}#tab=reviews".format(product_code)
 
+# url = "https://www.ceneo.pl/" + product_code + "#tab=reviews"
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 r = requests.get(url)
-print(r.status_code)
+
+if r.ok:
+    page_dom = BeautifulSoup(r.text, 'html.parser')
+    opinions = page_dom.select("div.js_product-review")
+
+    opinions_all = []
+    for opinion in opinions:
+        single_opinion = {
+            "opinion_id": opinion['data-entry-id'],
+            "author": opinion.select_one('span.user-post__author-name').get_text().strip(),
+            "recommendation": opinion.select_one('span.user-post__author-recomendation > em').get_text().strip(),
+            "score": opinion.select_one('span.user-post__score-count').get_text().strip(),
+            "purchased": opinion.select_one('div.review-pz').get_text().strip(),
+            "opinion_date": opinion.select_one('span.user-post__published > time:nth-child(1)')['datetime'].strip(),
+            "purchase_date": opinion.select_one('span.user-post__published > time:nth-child(2)')['datetime'].strip(),
+            "likes": opinion.select_one('button.vote-yes')['data-total-vote'].strip(),
+            "dislikes": opinion.select_one('button.vote-no')['data-total-vote'].strip(),
+            "content": opinion.select_one('div.user-post__text').get_text().strip(),
+            "cons": [tag.text.strip() for tag in opinion.select('div.review-feature__title--negatives ~ div.review-feature__item')],
+            "pros": [tag.text.strip() for tag in opinion.select('div.review-feature__title--positives ~ div.review-feature__item')]
+        }
+        opinions_all.append(single_opinion)
+    print(json.dumps(opinions_all, indent=4, ensure_ascii=False))
